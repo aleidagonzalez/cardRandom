42dbfda2fd718b10d4c248bfe60a980246fab56c
aleidagonzalez <aleida1983@hotmail.com>
2021-03-22 18:34:47 +0000

    mi card

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  var suits = ["♦", "♠", "♣", "♥"];
+
+  var value = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  var cardSuits = suits[Math.floor(Math.random() * 4)];
+  var cardValue = value[Math.floor(Math.random() * 13)];
+  document.getElementById("topSuit").innerHTML = cardSuits;
+  document.getElementById("bottomSuit").innerHTML = cardSuits;
+  document.querySelector(".number").innerHTML = cardValue;
 };
